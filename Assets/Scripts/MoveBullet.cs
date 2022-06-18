d44e606a4e2e71d7a48098f5d9160f353fc53989
H570-PLUS\masashi <hatano153@gmail.com>
2022-06-18 18:51:01 +0900

    add movebullet function for sphere

---
+++ Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{   
+    private Rigidbody rb;
+    private float speed = 30f;
+    private Vector3 direction = new Vector3(0,-1,-2);
+    private float max_dis = 300f;
+    private Vector3 pos_start = new Vector3(0,150,200);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = direction*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector3 pos_curr = transform.position;
+        float dis = Vector3.Distance(pos_start, pos_curr);
+        if (dis > max_dis)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
